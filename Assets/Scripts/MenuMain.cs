93fa4667ccda9393ed6942db64ec1786962e2b10
NortKron <as2631@hotmail.com>
2019-10-26 22:55:59 +0500

    Project created

---
+++ Assets/Scripts/MenuMain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class MenuMain : MonoBehaviour
+{
+    public void StartPressed()
+    {
+        Debug.Log("Start pressed!");
+        SceneManager.LoadScene("Level1");
+    }
+    public void ExitPressed()
+    {
+        Debug.Log("Quit pressed!");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    bool isGrounded = true;
+
+    int Lives = 10; 
+    public float speed = 1.0f;
+    public float jumpForce = 15.0f;
+
+    new Rigidbody2D rigidbody;
+    Animator animator;
+    SpriteRenderer sprite;
+
+    void Run()
+    {
+         Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+
+        sprite.flipX = direction.x > 0.0f;
+    }
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetButton("Horizontal")) Run();
+       if (isGrounded && Input.GetButtonDown("Jump")) Jump();
+    }
+
+    void Jump()
+    {
+        rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGrounded = colliders.Length > 1;
+    }
+
+}
